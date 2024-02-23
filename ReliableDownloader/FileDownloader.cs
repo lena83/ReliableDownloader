b54eef6eab9b61abbde416c303f4315dc4c6bb08
lena83 <lenpetrova@gmail.com>
2024-02-23 20:12:47 +0000

    Add files via upload
    initial commit

---
+++ ReliableDownloader/FileDownloader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReliableDownloader;
+
+public class FileDownloader : IFileDownloader
+{
+    public Task<bool> TryDownloadFile(
+        string contentFileUrl,
+        string localFilePath,
+        Action<FileProgress> onProgressChanged,
+        CancellationToken cancellationToken) => throw new NotImplementedException();
+}
---
+++ ReliableDownloader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReliableDownloader;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // If this url 404's, you can get a live one from https://installer.demo.accurx.com/chain/latest.json.
+        var exampleUrl = "https://installer.demo.accurx.com/chain/4.22.50587.0/accuRx.Installer.Local.msi";
+        var exampleFilePath = Path.Combine(Directory.GetCurrentDirectory(), "myfirstdownload.msi");
+        var fileDownloader = new FileDownloader();
+        var didDownloadSuccessfully = await fileDownloader.TryDownloadFile(
+            exampleUrl,
+            exampleFilePath,
+            progress => Console.WriteLine($"Percent progress is {progress.ProgressPercent}"),
+            CancellationToken.None);
+        Console.WriteLine($"File download ended! Success: {didDownloadSuccessfully}");
+    }
+}
